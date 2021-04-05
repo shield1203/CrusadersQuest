9c08d3e504a61909fc13bf6c76965fc230b093ac
KimJiHo <shield1203@naver.com>
2021-04-05 19:15:05 +0900

    JsonHelper : JSON배열을 파싱하기 위해 구현, 서버로부터 받는 JSON데이터와 캐릭터 정보 또는 맵정보등 앞으로의 여러 데이터관리를 JSON배열을 통해 관리.

---
+++ Assets/Scripts/Utility/JsonHelper.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Newtonsoft.Json.Linq;
+using UnityEngine;
+
+public static class JsonHelper
+{
+    public static List<T> FromJson<T>(string json)
+    {
+        List<T> DataArray = new List<T>();
+        
+        var dataArrayJson = JArray.Parse(json);
+        for(int index = 0; index < dataArrayJson.Count; index++)
+        {
+            T addData = JsonUtility.FromJson<T>(dataArrayJson[index].ToString());
+            DataArray.Add(addData);
+        }
+        
+        return DataArray;
+    }
+}
